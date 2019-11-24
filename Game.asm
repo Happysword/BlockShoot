02ddbd67d0e8a395c716a0c983dfa98edf6aceec
Happysword <45149700+Happysword@users.noreply.github.com>
2019-11-24 21:08:25 +0200

    Added GameNameIntro

--- Game.asm
+++ Game.asm
@@ -1,7 +1,248 @@
 .Model Large
 .Stack 64
 .Data
+IntroMessage db 'Press Space To Continue','$'
 .code
+
+GameName proc far
+	mov cx,223
+	mov dx,140
+	mov al,9
+	mov ah,0ch;parameters of draw pixel interrupt
+	mov si,21 ;thickness of line in pixels
+	
+	verticalup:
+		mov di,201 ;length of line
+		verticalupinside:	
+					push cx
+					int 10h
+					add cx,80
+					int 10h
+					add cx,40
+					int 10h
+					add cx,120
+					int 10h
+					add cx,80
+					int 10h
+					add cx,40
+					int 10h
+					add cx,120
+					int 10h
+					pop cx
+					inc dx
+					dec di
+					jnz verticalupinside
+		inc cx
+		mov dx,140
+		dec si
+		jnz verticalup
+	
+	
+	mov cx,223
+	mov dx,140
+	mov al,9
+	mov ah,0ch;parameters of draw pixel interrupt
+	mov si,21 ;thickness of line in pixels
+	horizontalup:
+		mov di,101;width of line 
+		horizontalupinside:
+					push cx 
+					push dx
+					int 10h
+					add dx,80
+					int 10h
+					add dx,100
+					int 10h
+					add cx,120
+					int 10h
+					add cx,120
+					int 10h
+					sub dx,180
+					int 10h
+					add cx,120
+					int 10h
+					add dx,180
+					int 10h
+					add cx,120
+					sub dx,80
+					int 10h
+					pop dx
+					pop cx
+					inc cx
+					dec di
+					jnz horizontalupinside
+		mov cx,223
+		inc dx
+		dec si
+		jnz horizontalup
+		
+	mov cx,244
+	mov dx,226
+	mov al,0
+	mov si,8
+	Bloop:
+		mov di,81
+		Bloopinside:
+					int 10h
+					inc cx
+					dec di
+					jnz Bloopinside
+		mov cx,243
+		inc dx
+		dec si
+		jnz Bloop
+		
+		
+					
+	mov cx,783
+	mov dx,140
+	mov al,9
+	mov ah,0ch;parameters of draw pixel interrupt
+	mov si,21 ;thickness of line in pixels	 
+	kloop:
+		mov di,101
+		kloopinside:
+					push cx 
+					push dx
+					int 10h
+					sub cx,10
+					add dx,100
+					int 10h
+					pop dx
+					pop cx
+					inc dx
+					dec di
+					jnz kloopinside
+		mov dx,140
+		inc cx
+		dec si
+		jnz kloop
+		
+		
+		
+	mov cx,223
+	mov dx,390
+	mov al,4
+	mov ah,0ch;parameters of draw pixel interrupt
+	mov si,21 ;thickness of line in pixels
+	
+	verticaldown:
+		mov di,201 ;length of line
+		verticaldowninside:	
+					push cx
+					add cx,120
+					int 10h
+					add cx,80
+					int 10h
+					add cx,40
+					int 10h
+					add cx,80
+					int 10h
+					add cx,40
+					int 10h
+					add cx,80
+					int 10h
+					add cx,80
+					int 10h
+					pop cx
+					inc dx
+					dec di
+					jnz verticaldowninside
+		inc cx
+		mov dx,390
+		dec si
+		jnz verticaldown	
+		
+		
+	mov cx,223
+	mov dx,390
+	mov al,4
+	mov ah,0ch;parameters of draw pixel interrupt
+	mov si,21 ;thickness of line in pixels
+	horizontaldown:
+		mov di,101;width of line 
+		horizontaldowninside:
+					push cx 
+					push dx
+					int 10h
+					add dx,80
+					int 10h
+					add dx,100
+					int 10h
+					add cx,120
+					sub dx,100
+					int 10h
+					add cx,120
+					sub dx,80
+					int 10h
+					add dx,180
+					int 10h
+					add cx,120
+					int 10h
+					sub dx,180
+					int 10h
+					add cx,120
+					int 10h
+					pop dx
+					pop cx
+					inc cx
+					dec di
+					jnz horizontaldowninside
+		mov cx,223
+		inc dx
+		dec si
+		jnz horizontaldown
+
+
+	mov cx,223
+	mov dx,390
+	mov al,4
+	mov ah,0ch;parameters of draw pixel interrupt
+	mov si,21 ;thickness of line in pixels	
+
+	Sloop:
+		mov di,101
+		Sloopinside:
+					push cx 
+					push dx
+					int 10h
+					add cx,80
+					add dx,100
+					int 10h
+					pop dx
+					pop cx
+					inc dx
+					dec di
+					jnz Sloopinside
+		mov dx,390
+		inc cx
+		dec si
+		jnz Sloop
+		
+	mov ah,2   ;move cursor
+	mov dx,2935h
+	mov bx,0
+	int 10h
+	
+	mov ah,9
+	lea dx,IntroMessage
+	int 21h
+		
+	;wait for key to exit
+WaitforSpace:	
+				mov ah,07;clear key from buffer
+				int 21h
+				cmp al,32
+				jnz WaitforSpace
+				
+	;clear screen
+ExitIntro:	mov ax, 4f02h
+			mov bx, 105h
+			int 10h
+
+	ret
+GameName endp
+
 Main proc far
 	mov ax,@data
 	mov ds,ax ; copy data segment
@@ -10,7 +251,7 @@ Main proc far
 	mov bx, 105h
 	int 10h ;graphical mode interrupt
 	
-	
+	Call GameName
 	
 	
 	
@@ -18,5 +259,5 @@ Main proc far
 	int 21h  ;return program to dos
 	
 Main endp
-end main
-	
+
+end main 
